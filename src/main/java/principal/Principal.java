141f3664c5b8ab001c5d7050df91df1d277e6c0c
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-09-16 10:45:26 -0300

    init: Início do projeto

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+import visao.FrmPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmPrincipal janela = new FrmPrincipal();
+        janela.setVisible(true);
+    }
+}
